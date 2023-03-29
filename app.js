bfdd4ccb77ffd2fafa61a6f75c3421885e04ab3a
Jorge Flores <flores.jorgeluis@gmail.com>
2023-03-28 18:54:31 -0600

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const expensesRoute = require("./routes/expenses");
+var cors = require("cors");
 
 dotenv.config();
+
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 const app = express();
